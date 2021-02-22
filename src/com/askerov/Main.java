741526bd8991ca3f56d330c90aa1fc93455338f7
123-kana <umar972511@gmail.com>
2021-02-22 07:45:44 +0600

    initial

---
+++ src/com/askerov/Main.java
@@ -0,0 +1,38 @@
+package com.askerov;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String myNameis = "men9"+" " + "zovut" +" "+"Kanatbek" + " " + "mne" +" "+ "23"+" "+ "goda";// String
+	System.out.println(myNameis);
+
+	myNameis = "Weclome" + " Kanatbek";
+        System.out.println(myNameis);
+
+
+        final int NUM; // NUM
+        NUM = 15;
+        System.out.println(NUM);
+
+        String word = "документов"; // word
+        System.out.println(word);
+
+        int slova;
+        System.out.println(NUM+" "+word);
+
+
+        if (NUM < 0){
+            if (NUM > 0){
+
+            }System.out.println("Вы сохранили отрицательное число");
+        }else
+            System.out.println("Вы сохранили нуль");
+
+    }
+
+
+
+
+}
